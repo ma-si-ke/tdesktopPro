87667902ef11e63649ed40f1a13d7e9840cb4348
23rd <23rd@vivaldi.net>
2026-04-22 13:35:47 +0300

    Added Add-to-Emoji-Set option to menu for small packless stickers.
    (cherry picked from commit cbef2a0cfc59e5e4607b48215d9cbe04b5eab643)

--- Telegram/SourceFiles/history/history_inner_widget.cpp
+++ Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -3226,7 +3226,7 @@ void HistoryInner::showContextMenu(QContextMenuEvent *e, bool showFromTouch) {
 								showStickerPackInfo(document);
 							}, &st::menuIconStickers);
 						} else {
-							Api::AddAddToStickerSetAction(
+							Api::AddAddToOwnedSetAction(
 								Ui::Menu::CreateAddActionCallback(_menu),
 								_controller->uiShow(),
 								document);
