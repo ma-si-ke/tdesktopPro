71952130efbf94dfd39b85c497045ecd1aac93ab
23rd <23rd@vivaldi.net>
2026-04-27 08:16:16 +0300

    Fixed off-center plus icon in add sticker cell of own sticker set box.

--- Telegram/SourceFiles/boxes/sticker_set_box.cpp
+++ Telegram/SourceFiles/boxes/sticker_set_box.cpp
@@ -2469,8 +2469,9 @@ void StickerSetBox::Inner::paintAddCell(QPainter &p) const {
 			ltrRect.width(),
 			ltrRect.height())
 		: ltrRect;
+	const auto center = rect::center(rect);
 	const auto inner = QRect(
-		rect::center(rect) - QPoint(
+		center - QPoint(
 			st::stickersAddCellBgRadius,
 			st::stickersAddCellBgRadius),
 		Size(st::stickersAddCellBgRadius * 2));
@@ -2486,7 +2487,6 @@ void StickerSetBox::Inner::paintAddCell(QPainter &p) const {
 
 	const auto plusHalf = st::stickersAddCellPlusSize / 2;
 	const auto thickness = st::stickersAddCellPlusThickness;
-	const auto center = rect.center();
 	const auto plusH = QRectF(
 		center.x() - plusHalf,
 		center.y() - thickness / 2.,
