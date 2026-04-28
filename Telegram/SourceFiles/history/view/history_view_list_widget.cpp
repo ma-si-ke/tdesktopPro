458516d226777ec26bbc40d927088fc096974d87
John Preston <johnprestonmail@gmail.com>
2026-04-28 18:14:27 +0700

    Fix quote in topic messages replies.
    Fixes #30621.

--- Telegram/SourceFiles/history/view/history_view_list_widget.cpp
+++ Telegram/SourceFiles/history/view/history_view_list_widget.cpp
@@ -2755,9 +2755,9 @@ SelectedItems ListWidget::getSelectedItems() const {
 	return collectSelectedItems();
 }
 
-const TextSelection &ListWidget::getSelectedTextRange(
+TextSelection ListWidget::getSelectedTextRange(
 		not_null<HistoryItem*> item) const {
-	return _selectedTextRange;
+	return (_selectedTextItem == item) ? _selectedTextRange : TextSelection();
 }
 
 int ListWidget::findItemIndexByY(int y) const {
