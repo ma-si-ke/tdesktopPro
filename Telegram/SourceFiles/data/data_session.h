9267d11917aefbcf50123f36a39fe6b9289f3de0
John Preston <johnprestonmail@gmail.com>
2026-05-15 14:34:56 +0400

    Update API scheme to layer 226. Guard-bots.
    (cherry picked from commit 5c5bacaf419ecf16c57d20517bd2231d17f8f8af)

--- Telegram/SourceFiles/data/data_session.h
+++ Telegram/SourceFiles/data/data_session.h
@@ -967,6 +967,14 @@ public:
 	void webViewResultSent(WebViewResultSent &&sent);
 	[[nodiscard]] rpl::producer<WebViewResultSent> webViewResultSent() const;
 
+	struct JoinChatWebViewDecision {
+		PeerId peerId;
+		uint64 queryId = 0;
+		MTPJoinChatBotResult result;
+	};
+	void joinChatWebViewDecision(JoinChatWebViewDecision &&decision);
+	[[nodiscard]] rpl::producer<JoinChatWebViewDecision> joinChatWebViewDecision() const;
+
 	void saveViewAsMessages(not_null<Forum*> forum, bool viewAsMessages);
 
 	[[nodiscard]] auto peerDecorationsUpdated() const
@@ -1367,6 +1375,7 @@ private:
 	base::flat_map<not_null<PeerData*>, MTP::DcId> _peerStatsDcIds;
 
 	rpl::event_stream<WebViewResultSent> _webViewResultSent;
+	rpl::event_stream<JoinChatWebViewDecision> _joinChatWebViewDecision;
 
 	rpl::event_stream<not_null<PeerData*>> _peerDecorationsUpdated;
 	base::flat_map<
