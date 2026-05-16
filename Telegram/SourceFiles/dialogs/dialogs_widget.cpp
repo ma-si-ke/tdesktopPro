f8e4463f843364f7789bf79aeaca423307195e2e
23rd <23rd@vivaldi.net>
2026-05-16 10:17:08 +0300

    Froze top bar suggestion content during child-list collapse animation.
    (cherry picked from commit 1c69103fc8c02701eaeef6941f57109e92c4d26f)

--- Telegram/SourceFiles/dialogs/dialogs_widget.cpp
+++ Telegram/SourceFiles/dialogs/dialogs_widget.cpp
@@ -1124,7 +1124,8 @@ void Widget::setupTopBarSuggestions() {
 				_innerList,
 				&session(),
 				std::move(on),
-				_childListShown.value());
+				_childListShown.value(),
+				_prepareTopBarSnapshot.events());
 		}) | rpl::flatten_latest() | rpl::on_next([=](
 				Ui::SlideWrap<Ui::RpWidget> *raw) {
 			if (raw) {
@@ -3468,6 +3469,7 @@ void Widget::openChildList(
 		}
 	}, shadow->lifetime());
 
+	_prepareTopBarSnapshot.fire({});
 	updateControlsGeometry();
 	updateControlsVisibility(true);
 
