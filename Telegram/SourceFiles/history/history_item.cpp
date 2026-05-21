d0457704ae615717c4ee281412f74425875054fd
John Preston <johnprestonmail@gmail.com>
2026-05-21 21:18:36 +0400

    Keep webpage_id inside CustomUrl entity data.
    (cherry picked from commit 615abf6ef31b4d394a0b8991f47cdd807a4a9e08)

--- Telegram/SourceFiles/history/history_item.cpp
+++ Telegram/SourceFiles/history/history_item.cpp
@@ -141,10 +141,9 @@ template <typename T>
 	return false;
 }
 
-[[nodiscard]] auto ParsedRichPage(
+[[nodiscard]] std::shared_ptr<const Iv::RichPage> ParsedRichPage(
 		not_null<Main::Session*> session,
-		const MTPRichMessage *richMessage)
--> std::shared_ptr<const Iv::RichPage> {
+		const MTPRichMessage *richMessage) {
 	return richMessage ? Iv::ParseRichPage(session, *richMessage) : nullptr;
 }
 
@@ -4111,7 +4110,7 @@ void HistoryItem::setText(TextWithEntities textWithEntities) {
 		: std::move(textWithEntities));
 }
 
-auto HistoryItem::richPage() const -> std::shared_ptr<const Iv::RichPage> {
+std::shared_ptr<const Iv::RichPage> HistoryItem::richPage() const {
 	const auto source = Get<HistoryMessageRichPageSource>();
 	return source ? source->page : nullptr;
 }
