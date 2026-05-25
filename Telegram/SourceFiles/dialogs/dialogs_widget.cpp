2a3e59e3af2e9e0c62232e3b17ba1a94107ea2c1
John Preston <johnprestonmail@gmail.com>
2026-05-25 15:07:50 +0400

    Don't show full topics list for trivial searches.
    (cherry picked from commit e76abf577656948c72c484a0c4deca0a6922e111)

--- Telegram/SourceFiles/dialogs/dialogs_widget.cpp
+++ Telegram/SourceFiles/dialogs/dialogs_widget.cpp
@@ -2803,8 +2803,8 @@ bool Widget::peerSearchRequired() const {
 bool Widget::searchForTopicsRequired(const QString &query) const {
 	return _searchState.filterChatsList()
 		&& _openedForum
-		&& !query.isEmpty()
 		&& (IsHashOrCashtagSearchQuery(query) == HashOrCashtag::None)
+		&& !TextUtilities::PrepareSearchWords(query).isEmpty()
 		&& !_openedForum->topicsList()->loaded();
 }
 
