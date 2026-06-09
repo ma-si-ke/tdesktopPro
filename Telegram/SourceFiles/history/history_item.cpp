77c814bd81298a0621109072dee088e6e2905eec
John Preston <johnprestonmail@gmail.com>
2026-06-09 12:43:54 +0400

    Disable IV editor for now.
    (cherry picked from commit 9a234182e1e8b3e099719404378b10f374a21731)

--- Telegram/SourceFiles/history/history_item.cpp
+++ Telegram/SourceFiles/history/history_item.cpp
@@ -21,7 +21,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "history/history_unread_things.h"
 #include "history/history.h"
 #include "iv/iv_data.h"
-#include "iv/editor/iv_editor_state.h"
 #include "iv/iv_rich_page.h"
 #include "mtproto/mtproto_config.h"
 #include "ui/text/format_values.h"
@@ -4242,7 +4241,7 @@ void HistoryItem::setRichPage(std::shared_ptr<const Iv::RichPage> page) {
 			++source->fullPageVersion;
 		}
 		source->fullPage = nullptr;
-		source->canEdit = Iv::Editor::CanEditRichPage(source->page);
+		source->canEdit = false;
 		media->url = QString();
 		media->documents.clear();
 		media->photos.clear();
@@ -4258,7 +4257,7 @@ void HistoryItem::setFullRichPage(std::shared_ptr<const Iv::RichPage> page) {
 		AddComponents(HistoryMessageRichPageSource::Bit());
 		const auto source = Get<HistoryMessageRichPageSource>();
 		source->fullPage = std::move(page);
-		source->canEdit = Iv::Editor::CanEditRichPage(BestRichPage(source));
+		source->canEdit = false;
 	} else {
 		clearFullRichPage();
 	}
@@ -4272,7 +4271,7 @@ void HistoryItem::clearFullRichPage() {
 	++source->fullPageVersion;
 	source->fullPage = nullptr;
 	if (source->page) {
-		source->canEdit = Iv::Editor::CanEditRichPage(source->page);
+		source->canEdit = false;
 	} else {
 		RemoveComponents(HistoryMessageRichPageSource::Bit());
 	}
